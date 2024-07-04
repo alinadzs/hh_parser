25e8afb2fe339fe4193a99ffc32423f83fd9ddfd
alinadzs <al@>
2024-07-04 23:03:31 +0300

    парсер

---
+++ config.py
@@ -0,0 +1,8 @@
+token = '7334460430:AAGICfuxkR_bu2OEjgw7L0MvvaLsl6mBeAM'
+
+help_text = (
+    "Команды:\n"
+    "/start - Начать работу с ботом\n"
+    "/help - Получить помощь\n"
+    "/vacancies - Получить список вакансий"
+)
